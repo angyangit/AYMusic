ad7fb6fe50cc72756445684b5a63f7430960c9e7
angyang <angyangit@163.com>
2017-11-12 16:42:02 +0800

    第一次提交

--- app/src/main/java/com/example/angyang/aymusic/view/activity/MainActivity.java
+++ app/src/main/java/com/example/angyang/aymusic/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.angyang.aymusic;
+package com.example.angyang.aymusic.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.angyang.aymusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
