02bf959783e90730732f561906b786cb09bb5278
angyang <angyangit@163.com>
2017-11-22 23:19:12 +0800

    分包

--- app/src/main/java/com/example/angyang/aymusic/view/activity/MainActivity.java
+++ app/src/main/java/com/example/angyang/aymusic/view/activity/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.angyang.aymusic.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.angyang.aymusic.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv= (ImageView) findViewById(R.id.iv);
+
+
     }
 }
